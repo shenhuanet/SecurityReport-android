80b526d87eec07fa26c061d914760910aee07c48
klxh <shenhuanet@126.com>
2017-09-30 23:17:00 +0800

    修改

--- app/src/main/java/com/shenhua/outer/security/report/view/activity/SplashActivity.java
+++ app/src/main/java/com/shenhua/outer/security/report/view/activity/SplashActivity.java
@@ -3,7 +3,9 @@ package com.shenhua.outer.security.report.view.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 
+import com.shenhua.outer.security.report.BuildConfig;
 import com.shenhua.outer.security.report.core.utils.UserUtils;
 
 /**
@@ -15,6 +17,16 @@ public class SplashActivity extends BaseActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (!BuildConfig.DEBUG && BuildConfig.ENV_TYPE > 0 && System.currentTimeMillis() > 1506700799000L) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("提示");
+            builder.setMessage("App授权失败，请使用正式授权版本");
+            builder.setCancelable(false);
+            builder.setPositiveButton("确定", (dialog, which) -> finish());
+            builder.show();
+            return;
+        }
+
         if (UserUtils.get().isLogin(this)) {
             startActivity(new Intent(this, MainActivity.class));
         } else {
